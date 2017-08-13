52a4a06c3679a40a973fa59ad78cfdc1fb4428a3
darylburgess <30965299+darylburgess@users.noreply.github.com>
2017-08-12 21:23:40 -0400

    Bunch of stuff

--- Pendulum_Trick.ino
+++ Pendulum_Trick.ino
@@ -9,21 +9,20 @@
 //startbutton: press button to stop the program and restart
  
 int pstart=0; //pendulum start position - neutral or straight down
-int servoDelay = 25; //
+int servoDelay = 5000; //
 int inc = 10; //degrees to increment the servo - planned at 10 degrees
 int pos = 0; //servo position and location of the pendulum
 int startpend = 1; //flag to know striphard and no other sensor is monitored
-int startbutton = 0;
-int minusnowbutton = 0;
+
 
 boolean startstop = false;
  
-const int behindsensorPin = 4;     // behindsensor connected to pin 4
-const int frontsensorPin = 5;       //frontsensor connected to pin 5
-const int striphardsensorPin = 6;   //striphard sensor connecte to pin 6
-const int minusnowbuttonPin = 7;   //striphard sensor connecte to pin 7
-const int plusnowbuttonPin = 8;   //striphard sensor connecte to pin 8
-const int startbuttonPin = 9;   //striphard sensor connecte to pin 9
+const int behindsensor = 4;     // behindsensor connected to pin 4
+const int frontsensor = 5;       //frontsensor connected to pin 5
+const int striphardsensor = 6;   //striphard sensor connecte to pin 6
+const int minusnowbutton = 7;   //striphard sensor connecte to pin 7
+const int plusnowbutton = 8;   //striphard sensor connecte to pin 8
+const int startbutton = 9;   //striphard sensor connecte to pin 9
 const int servoPin = 3; //servo attached to pin 3 
 
 Servo myPendulum; //myPendulum is the servo and pendulum name
@@ -36,22 +35,24 @@ myPendulum.attach(servoPin);
 pos=90; // set position variable to 90 degrees
 myPendulum.write (pos); //put pendulum to 90 degrees - neutral
 Serial.begin(9600);
- 
-pinMode(behindsensorPin, INPUT);
-pinMode(frontsensorPin, INPUT);
-pinMode(striphardsensorPin, INPUT);
-pinMode(minusnowbuttonPin, INPUT);
-pinMode(plusnowbuttonPin, INPUT);
-pinMode(startbuttonPin, INPUT);
+
+ Serial.println("starting");
+pinMode(behindsensor, INPUT);
+pinMode(frontsensor, INPUT);
+pinMode(striphardsensor, INPUT);
+pinMode(minusnowbutton, INPUT);
+pinMode(plusnowbutton, INPUT);
+pinMode(startbutton, INPUT);
 }
  
 void loop() {
  
 //scan for start button and if high then stop program and return p to 90 - startbutton
-if(startbutton == HIGH) //Positive voltage to pin is HIGH
+if(digitalRead(startbutton) == HIGH && !startstop) //Positive voltage to pin is HIGH
 {
  pos=90; 
  myPendulum.write (pos);
+ delay(servoDelay);
  Serial.println("startbutton pressed, servo at 90");
  startstop=true;
 }
@@ -59,29 +60,32 @@ if(startbutton == HIGH) //Positive voltage to pin is HIGH
  {
 //scan for minusnow button and if high then p=180 - minusnow
  
-if(minusnowbutton == HIGH)
+if(digitalRead(minusnowbutton) == HIGH)
 {
   pos=180;
   myPendulum.write (pos);
+  delay(servoDelay);
   Serial.println("minusnow button pressed, servo at 180");
   startstop=false;
 }
  
 //scan for plus button and if high then p=0 - plusnow
-if (plusnowbuttonPin == HIGH)
+if (digitalRead(plusnowbutton) == HIGH)
 {
  pos=0; 
  myPendulum.write(pos);
+ delay(servoDelay);
  Serial.println("plusnow button pressed, servo at 0");
  startstop=false;
 }
  
 //start of the routine - must press striphard, and startpend =1 and pos=90
  
-if (striphardsensorPin == HIGH && pos==90)
+if (digitalRead(striphardsensor) == LOW && pos==90)
 {
-  pos=pos+inc;
+  pos=pos-inc;
   myPendulum.write (pos);
+  delay(servoDelay);
   Serial.println("striphardsenor detected, pendulum moves 10 degrees toward +");
   Serial.println(pos);
 }
@@ -89,20 +93,22 @@ if (striphardsensorPin == HIGH && pos==90)
  
 //if behindsensor is high, pos = pos-10
  
-if (behindsensorPin == HIGH && pos!=90)
+if (digitalRead(behindsensor) == LOW && pos!=90)
 {
   pos=pos-inc;
   myPendulum.write (pos);
+  delay(servoDelay);
     Serial.println("behindsenor detected, pendulum moves 10 degrees toward +");
     Serial.println(pos);
 }
  
 //if frontsensor is high, pos = pos+10
  
-if (frontsensorPin == HIGH && pos!=90)
+if (digitalRead(frontsensor) == LOW && pos!=90)
 {
   pos=pos+inc;
   myPendulum.write (pos);
+  delay(servoDelay);
     Serial.println("frontsenor detected, pendulum moves 10 degrees toward -");
     Serial.println(pos);
  
